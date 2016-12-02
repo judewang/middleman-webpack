c5e60f739c51e0c7782e46b64e20bd0184a775ff
judewang <jude@jude.asia>
2016-12-03 01:23:17 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# middleman-webpack
+
+A middleman template using webpack to replace middleman asset pipeline.
+
+## Requirements
+
+- NodeJS
+- Ruby
+- Bundler installed
+- npm or yarn installed
+
+## TODO
+
+- Update README
+- Update credits
---
+++ config.rb
@@ -0,0 +1,73 @@
+###
+# Page options, layouts, aliases and proxies
+###
+
+# Per-page layout changes:
+#
+# With no layout
+page '/*.xml', layout: false
+page '/*.json', layout: false
+page '/*.txt', layout: false
+
+# With alternative layout
+# page "/path/to/file.html", layout: :otherlayout
+
+# Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
+# proxy "/this-page-has-no-template.html", "/template-file.html", locals: {
+#  which_fake_page: "Rendering a fake page with a local variable" }
+
+###
+# Helpers
+###
+
+# Methods defined in the helpers block are available in templates
+# helpers do
+#   def some_helper
+#     "Helping"
+#   end
+# end
+
+# General configuration
+
+Slim::Engine.options[:pretty] = true
+
+# Run webpack with external_pipeline
+
+activate :external_pipeline,
+  name: :webpack,
+  command: build? ? 'npm run build' : 'npm run dev-server',
+  source:  '.webpack',
+  latency: 0
+
+# Reload the browser automatically whenever files change
+configure :development do
+  activate :livereload
+end
+
+# Build-specific configuration
+configure :build do
+
+  case ENV['TARGET'].to_s.downcase
+  when 'staging'
+    activate :asset_hash
+  else
+    set :build_dir, 'dist'
+  end
+
+end
+
+# Deploy configurations
+case ENV['TARGET'].to_s.downcase
+when 'staging'
+  activate :deploy do |deploy|
+    deploy.deploy_method = :git
+    deploy.remote = 'github-repository'
+    deploy.branch = 'gh-pages'
+  end
+else
+  activate :deploy do |deploy|
+    deploy.deploy_method = :git
+    deploy.remote = 'git-lab or other repository'
+    deploy.branch = 'static'
+  end
+end
